65592ac3955f4bd8676dfae92f70f1efcac92e90
Jesus David <jbernalme@gmail.com>
2020-11-03 12:11:42 -0500

    Agregando ubicacion mapa en el form

---
+++ resources/js/mapa.js
@@ -0,0 +1,113 @@
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+const provider = new OpenStreetMapProvider();
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Solo se ejecutara si en el documento existe un elemento con la etiqueta 'mapa'
+    if(document.querySelector('#mapa')){
+        const lat = 20.666332695977;
+        const lng = -103.392177745699;
+
+        const mapa = L.map('mapa').setView([lat, lng], 16);
+
+        // Eliminar pines previos
+        let markers = new L.FeatureGroup().addTo(mapa);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mapa);
+
+        let marker;
+
+        // agregar el pin
+        marker = new L.marker([lat, lng], {
+            draggable: true,
+            autoPan: true
+        }).addTo(mapa);
+
+        // Asignar el contenedor de markers el nuevo pin
+        markers.addLayer(marker);
+
+        // GeoCode service
+        const geocodeService = L.esri.Geocoding.geocodeService();
+
+        // Buscador de direcciones
+        const buscador = document.querySelector('#formbuscador');
+        buscador.addEventListener('blur', buscarDireccion);
+
+        // Detectar movimiento del marker
+        reubicarPin(marker);
+        function reubicarPin(marker) {
+            marker.on('moveend', function(e) {
+                marker = e.target;
+                const position = marker.getLatLng();
+
+                // Centar el mapa donde se soltó el pin
+                mapa.panTo( new L.LatLng(position.lat, position.lng));
+
+                // Reverse Geocoding, cuando el user reubica el pin
+                geocodeService.reverse().latlng(position, 16).run(function(error, resultado){
+
+                    marker.bindPopup(resultado.address.LongLabel);
+                    marker.openPopup();
+
+                    // Llenar campos form
+                    llenarInputs(resultado);
+
+                });
+            });
+        }
+        function buscarDireccion(e) {
+
+            if(e.target.value.length > 4){
+                provider.search({query: e.target.value })
+                    .then( resultado => {
+
+                        if (resultado[0]) {
+
+                            // Limpiar los pines previos
+                            markers.clearLayers();
+
+                            // Reverse Geocoding, cuando el user reubica el pin
+                            geocodeService.reverse().latlng(resultado[0].bounds[0], 16).run(function(error, resultado){
+
+
+                                // Llenar inputs
+                                llenarInputs(resultado);
+
+                                // Centrar mapa
+                                mapa.setView(resultado.latlng);
+
+                                // Agregar pin
+                                marker = new L.marker(resultado.latlng, {
+                                    draggable: true,
+                                    autoPan: true
+                                }).addTo(mapa);
+
+                                // Asignar el contenedor de markers el nuevo pin
+                                markers.addLayer(marker);
+
+                                // Mover pin
+                                reubicarPin(marker);
+
+                            });
+                        }
+
+                    }).catch( error => {
+                        console.log(error);
+                    })
+            }
+
+
+        }
+
+        function llenarInputs(resultado) {
+            document.querySelector('#direccion').value = resultado.address.Address || '';
+            document.querySelector('#barrio').value = resultado.address.Neighborhood || '';
+            document.querySelector('#lat').value = resultado.latlng.lat || '';
+            document.querySelector('#lng').value = resultado.latlng.lng || '';
+        }
+    }
+
+
+});
--- resources/views/establecimientos/create.blade.php
+++ resources/views/establecimientos/create.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""/>
+    <!-- Esri Leaflet Geocoder -->
+    <link
+    rel="stylesheet"
+    href="https://unpkg.com/esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css"
+    />
+@endsection
 @section('content')
     <div class="container">
         <h1 class="text-center mt-4">Resgistrar Establecimiento</h1>
@@ -78,10 +88,58 @@
                         >
                         <p class="text-secondary mt-5 mb-3 text-center">El asistente colocará una dirección estimada, mueve el pin hacia el lugar correcto</p>
                     </div>
+                    <div class="form-group">
+                        <div id="mapa" style="height: 400px"></div>
+                    </div>
+                    <p class="informacion">Confirma que los siguientes campos son correctos</p>
 
+                    <div class="form-group">
+                        <label for="direccion">Dirección</label>
+                        <input
+                            type="text"
+                            id="direccion"
+                            class="form-control @error('direccion') is-invalid @enderror"
+                            placeholder="Dirección"
+                            value="{{ old('direccion') }}"
+                        >
+                        @error('direccion')
+                            <div class="invalid-feedback">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="barrio">Barrio</label>
+                        <input
+                            type="text"
+                            id="barrio"
+                            class="form-control @error('barrio') is-invalid @enderror"
+                            placeholder="Barrio"
+                            value="{{ old('barrio') }}"
+                        >
+                        @error('barrio')
+                            <div class="invalid-feedback">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <input type="hidden" id="lat" name="lat" value="{{ old('lat') }}">
+                    <input type="hidden" id="lng" name="lng" value="{{ old('lng') }}">
                 </fieldset>
             </form>
         </div>
 
     </div>
+@endsection
+
+@section('scripts')
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+    crossorigin=""></script>
+    {{-- plugin ppal, para ultilizar todos --}}
+    <script src="https://unpkg.com/esri-leaflet" defer></script>
+
+    <!-- Esri Leaflet Geocoder -->
+    <script src="https://unpkg.com/esri-leaflet-geocoder" defer></script>
+
 @endsection
